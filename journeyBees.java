ff1181ac0b170167b0cf37f736d7bd8abe4f4b3f
Crow <crow.veal@gmail.com>
2024-05-14 00:48:54 -0400

    Create journeyBees.java

---
+++ journeyBees.java
@@ -0,0 +1,12 @@
+/*
++ Crow Practice Project
++ idea: bees
+	- different sizes of bees with different abilities
+	- oregon trail style
+	- 4 bees
+	- 6 stages
+		~ 3 food (budgeting)
+		~ 3 obstacles
+		~ 
+		
+*/
