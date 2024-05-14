840c6e62d2f324eb1a57fd33e189a2614e7d5e2d
Crow <crow.veal@gmail.com>
2024-05-14 05:10:03 -0400

    chose javafx over swing

--- bug journey/journey.java
+++ bug journey/journey.java
@@ -19,25 +19,57 @@
 //i actually hate bugs
 
 import java.util.*;
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 /*layout plan: 4 bugs along bottom with vector sprites, constant*/
 
-public class journey{
+public class journey extends Application{
 	public static void main(String[] args){
-		JFrame frame = new JFrame();//create window
-		
-		frame.setSize(470, 420);//x axis, y axis, width, height  
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage){
 		//each picture is 100, 10 between each 20 on each side
 		//width adds up to 400 + 30 + 40 = 470
 		//bottom section will have the photos with 100 height on each photo and 30 on top and 10 on bottom
 		//adds up to 140, top section will be double. total height is 140*3= 420
 		
+		//use flow layout for bottom photos
 		
+		//original pngs will be 405x405
 		
+		Image worm = new Image("file:worm.png");
+		ImageView viewWorm = new ImageView(worm);
+		viewWorm.setFitWidth(100);
+		viewWorm.setPreserveRatio(true);
+		Button wormPhoto = new Button();
+		wormPhoto.setGraphic(viewWorm);
 		
+		/*
+		left off plan (5:09am):
+			- make other vectors
+			- align hbox bottom center with padding
+			- start on bug credentials
+		*/
 		
-		frame.setVisible(true);//show window
+		HBox bugs = new HBox(viewWorm);
+		Scene scene = new Scene(bugs, 470, 420);
+      
+		// Add the Scene to the Stage.
+		primaryStage.setScene(scene);
 		
+		// Set the stage title.
+		primaryStage.setTitle("bug journey");
+		
+		// Show the window.
+		primaryStage.show();
 	}
 }
