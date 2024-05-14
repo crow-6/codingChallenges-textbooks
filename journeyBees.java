147bb8f092fd27e9f25335e115e564e84fa78e70
Crow <crow.veal@gmail.com>
2024-05-14 00:56:42 -0400

    Update journeyBees.java

--- journeyBees.java
+++ journeyBees.java
@@ -5,8 +5,12 @@
 	- oregon trail style
 	- 4 bees
 	- 6 stages
-		~ 3 food (budgeting)
-		~ 3 obstacles
-		~ 
-		
+		~ 3 food (budgeting due to growth)
+		~ 3 obstacles (success will determine how much food is won)
+		~ 2 riddles
+		~ ending
++ plan: have it be in a window and not command line
++ start date: may-14-2024-12:56am 
 */
+
+import java.util.*;
