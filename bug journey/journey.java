49f5afffc58da0295f07c4e1c5e509c63fa85149
Crow <crow.veal@gmail.com>
2024-05-14 23:24:36 -0400

    progress
    made buttons and vectors

--- bug journey/journey.java
+++ bug journey/journey.java
@@ -9,7 +9,7 @@
 	- 6 stages
 		~ 3 food (budgeting due to growth)
 		~ 3 obstacles (success will determine how much food is won)
-		~ 2 riddles
+		~ 3 riddles (success will determine how much food is won)
 		~ 2 enemies (non-bugs)
 		~ ending (party or other)
 + plan: have it be in a window and not command line
@@ -24,6 +24,8 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -38,30 +40,55 @@ public class journey extends Application{
 	@Override
 	public void start(Stage primaryStage){
 		//each picture is 100, 10 between each 20 on each side
-		//width adds up to 400 + 30 + 40 = 470
 		//bottom section will have the photos with 100 height on each photo and 30 on top and 10 on bottom
-		//adds up to 140, top section will be double. total height is 140*3= 420
-		
-		//use flow layout for bottom photos
 		
 		//original pngs will be 405x405
 		
 		Image worm = new Image("file:worm.png");
+		Image caterpillar = new Image("file:caterpillar.png");
+		Image ant = new Image("file:ant.png");
+		Image bee = new Image("file:bee.png");
+		
 		ImageView viewWorm = new ImageView(worm);
+		ImageView viewCaterpillar = new ImageView(caterpillar);
+		ImageView viewAnt = new ImageView(ant);
+		ImageView viewBee = new ImageView(bee);
+		
 		viewWorm.setFitWidth(100);
 		viewWorm.setPreserveRatio(true);
 		Button wormPhoto = new Button();
 		wormPhoto.setGraphic(viewWorm);
 		
+		//registering worm button to method
+		wormPhoto.setOnAction(new showWormInfo());
+		
+		viewCaterpillar.setFitWidth(100);
+		viewCaterpillar.setPreserveRatio(true);
+		Button caterpillarPhoto = new Button();
+		caterpillarPhoto.setGraphic(viewCaterpillar);
+		
+		viewAnt.setFitWidth(100);
+		viewAnt.setPreserveRatio(true);
+		Button antPhoto = new Button();
+		antPhoto.setGraphic(viewAnt);
+		
+		viewBee.setFitWidth(100);
+		viewBee.setPreserveRatio(true);
+		Button beePhoto = new Button();
+		beePhoto.setGraphic(viewBee);
+		
 		/*
 		left off plan (5:09am):
-			- make other vectors
-			- align hbox bottom center with padding
+			x - make other vectors
+			x - align hbox bottom center with padding
 			- start on bug credentials
+				idea: have doc with abilities that reads
 		*/
 		
-		HBox bugs = new HBox(viewWorm);
-		Scene scene = new Scene(bugs, 470, 420);
+		HBox bugs = new HBox(10, wormPhoto, caterpillarPhoto, antPhoto, beePhoto);
+		bugs.setPadding(new Insets(30, 20, 10, 20));//top, right, bottom, left
+		Scene scene = new Scene(bugs, 540, ((30+10+100)*3));
+		bugs.setAlignment(javafx.geometry.Pos.BOTTOM_CENTER);
       
 		// Add the Scene to the Stage.
 		primaryStage.setScene(scene);
@@ -72,4 +99,13 @@ public class journey extends Application{
 		// Show the window.
 		primaryStage.show();
 	}
+	
+	class showWormInfo implements EventHandler<actionevent>{
+		@Override
+		public void handle(ActionEvent event){
+			
+			
+		}
+	}
+	
 }
--- bug journey/readme.txt
+++ bug journey/readme.txt
@@ -2,3 +2,11 @@ I am Crow.
 I would like for it to be said in advance that I am not a biologist.
 I do not know the finer details of bug diets, alliances, or the like.
 this is for practice (and fun).
+
+---------------------------------------------------------------------
+
+This is a short sort of text adventure with 4 bugs.
+
+You will need to keep track of food, health, progress, and abilities
+
+Each bug will have 3 abilities you can use to solve the puzzles and defeat enemies
